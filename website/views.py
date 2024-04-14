62b3016ed53f2450452da1b8453f83a70c92788c
Ryan Baker <rbaker@xtier.com>
2024-04-14 09:06:59 -0400

    updated header and sign up page. Basic but functional.

--- website/views.py
+++ website/views.py
@@ -8,3 +8,8 @@ views = Blueprint("views", __name__)
 @views.route("/")
 def home():
     return render_template("home.html")
+
+
+@views.route("/forum")
+def forum():
+    return render_template("forum.html")
